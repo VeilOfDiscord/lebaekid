c6a49266cb4350ffe9f3a59f417be686bb70310a
Nathaell Avril Leman <84324372+VeilOfDiscord@users.noreply.github.com>
2025-11-02 03:17:38 +0700

    re-adjusted spacing and calendar list overflow

--- lebaekid/src/pages/calendar/Calendar.tsx
+++ lebaekid/src/pages/calendar/Calendar.tsx
@@ -16,8 +16,8 @@ const Calendar = () => {
           Calendar
         </h1>
 
-        <div className="flex flex-col md:grid md:grid-cols-3 justify-items-center text-center m-10 lg:mx-40 md:gap-20 gap-6">
-          <div className="bg-gray-200 rounded-xl aspect-square md:aspect-auto w-full px-6 md:col-span-2">
+        <div className="flex flex-col md:grid md:grid-cols-3 justify-items-center text-center md:m-10 lg:mx-40 md:gap-20 gap-6">
+          <div className="overflow-scroll md:overflow-auto bg-gray-200 rounded-xl md:aspect-auto w-full h-[500px] md:max-h-[1000px] md:h-auto px-6 md:col-span-2 ">
             <h2 className="pt-6 text-4xl font-extrabold leading-tight tracking-tight text-gray-900">
               Events
             </h2>
