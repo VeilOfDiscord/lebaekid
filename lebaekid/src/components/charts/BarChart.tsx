3b7a008ea02ccfdd2b4ed3aae0ba50310b4cae90
Nathaell Avril Leman <84324372+VeilOfDiscord@users.noreply.github.com>
2026-01-28 16:39:31 +0700

    Update to accomodate favorite votes to be shown

--- lebaekid/src/components/charts/BarChart.tsx
+++ lebaekid/src/components/charts/BarChart.tsx
@@ -20,7 +20,7 @@ const BarChart: React.FC<BarChartProps> = ({ sheetData }) => {
     BarElement,
     Title,
     Tooltip,
-    Legend
+    Legend,
   );
 
   const options = {
@@ -34,13 +34,14 @@ const BarChart: React.FC<BarChartProps> = ({ sheetData }) => {
     },
   };
   const filteredData = sheetData.filter((row: any) => {
-    return (row.P1 !== "" && row.P1 !== 0) || (row.P2 !== "" && row.P2 !== 0);
+    return (row.P1 !== "" && row.P1 !== 0) || (row.P2 !== "" && row.P2 !== 0)  || (row.F !== "" && row.F !== 0)
   });
 
   console.log("Filtered Data:", filteredData);
   const labels = filteredData.map((row: any) => row?.Peserta);
   const P1Data = filteredData.map((row: any) => row?.P1);
   const P2Data = filteredData.map((row: any) => row?.P2);
+  const FData = filteredData.map((row: any) => row?.F);
 
   const data = {
     labels: labels,
@@ -56,6 +57,14 @@ const BarChart: React.FC<BarChartProps> = ({ sheetData }) => {
       {
         label: "P2",
         data: P2Data,
+        backgroundColor: ["rgba(255, 99, 132, 0.5)"],
+        borderColor: ["rgba(255, 99, 132, 1)", "rgba(53, 162, 235, 1)"],
+        borderWidth: 1,
+        skipNull: true,
+      },
+      {
+        label: "Peserta Favorit",
+        data: FData,
         backgroundColor: ["rgba(255, 99, 132, 0.5)"],
         borderColor: ["rgba(255, 99, 132, 1)", "rgba(53, 162, 235, 1)"],
         borderWidth: 1,
